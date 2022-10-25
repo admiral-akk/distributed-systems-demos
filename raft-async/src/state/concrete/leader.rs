60974631dbd8a23497aef28705534e527bc927ee
Kuba Karpierz <mr.karpierz@gmail.com>
2022-10-25 12:55:54 -0400

    Added test for append fail

--- raft-async/src/state/concrete/leader.rs
+++ raft-async/src/state/concrete/leader.rs
@@ -342,4 +342,38 @@ mod tests {
         assert_eq!(leader.next_index[&0], 2);
         assert_eq!(leader.match_index[&0], 2);
     }
+
+    #[test]
+    fn test_append_response_fails() {
+        let config = Config {
+            servers: HashSet::from([0, 1, 2, 3, 4]),
+        };
+        let mut persistent_state: PersistentState<u32> = PersistentState {
+            config,
+            id: 1,
+            current_term: 3,
+            log: Vec::from([Entry { term: 1, data: 10 }, Entry { term: 3, data: 4 }]),
+            ..Default::default()
+        };
+        let mut volitile_state = VolitileState { commit_index: 1 };
+        let request: Request<u32> = Request {
+            sender: 0,
+            reciever: persistent_state.id,
+            term: 3,
+            event: Event::AppendResponse(request::AppendResponse { success: false }),
+        };
+
+        let mut leader = Leader {
+            next_index: HashMap::from([(0, 2), (2, 2), (3, 2), (4, 1)]),
+            match_index: HashMap::from([(0, 0), (2, 0), (3, 0), (4, 0)]),
+        };
+
+        let (requests, next) =
+            leader.handle_request(&mut volitile_state, &mut persistent_state, request);
+
+        assert!(next.is_none());
+        assert!(requests.is_empty());
+        assert_eq!(leader.next_index[&0], 1);
+        assert_eq!(leader.match_index[&0], 0);
+    }
 }
